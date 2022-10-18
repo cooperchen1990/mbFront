d5c72bc2a79a805dac218e7e46f6d8604b819095
Cooper <sssss888880@livemail.tw>
2022-10-19 00:13:44 +0800

    create mbFront

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <back-ground></back-ground>
+</template>
+
+<script>
+import BackGround from "@/components/BackGround";
+
+export default {
+  name: 'App',
+  components: {
+    BackGround
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/BackGround.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="background-image:url('http://localhost:8080/img/pic.png');width:100vh;height:100vh;"></div>
+</template>
+
+<script>
+export default {
+  name: "BackGround"
+}
+</script>
+
+<style scoped>
+
+</style>
